c03257d540496734a27862ea4afd2de9211f34a0
pysf <payam.ysfi@gmail.com>
2022-08-25 10:05:08 +0200

    refactor: use waitgroup instead of channel in simulator

--- internal/simulator/simulator.go
+++ internal/simulator/simulator.go
@@ -12,7 +12,6 @@ import (
 	"github.com/pysf/special-umbrella/internal/client/clienttype"
 	"github.com/pysf/special-umbrella/internal/config"
 	"github.com/pysf/special-umbrella/internal/scooter"
-	"github.com/pysf/special-umbrella/internal/simulator/simulatortype"
 )
 
 type Simulator struct {
@@ -46,221 +45,102 @@ func NewSimulator(ctx context.Context) *Simulator {
 func (s *Simulator) Start() {
 
 	go func() {
-		botResultsCh := s.RunBots()
-		for {
-			select {
-			case <-s.ctx.Done():
-				return
-			case sr, ok := <-botResultsCh:
-				if !ok {
-					return
-				}
-				if sr.Err != nil {
-					log.Printf("Simulator Err= %v \n", sr.Err)
-					return
-				}
-				log.Printf("Simulator: %v \n", sr.Message)
-			}
-		}
 
-	}()
-
-}
-
-func (s *Simulator) RunBots() chan simulatortype.SimulatorResponse {
-
-	resultChan := make(chan simulatortype.SimulatorResponse)
-
-	go func() {
-		defer close(resultChan)
 		time.Sleep(s.startDelay)
 		log.Println("Simulator started...")
 
 		searchResult, err := s.apiClient.FindScooters(s.bottomLeft, s.topRight, "available")
 		if err != nil {
-			s.SendResult(resultChan, simulatortype.SimulatorResponse{
-				Err: fmt.Errorf("StartSimulator: failed to fetch available scooters err= %w", err),
-			})
+			fmt.Println(fmt.Errorf("StartSimulator: failed to fetch available scooters err= %w", err))
 			return
 		}
 		scooters := searchResult.Scooters
 		log.Printf("%v scooters are available \n", len(scooters))
 
-		scooterCh := make(chan clienttype.Scooter, len(scooters))
-		defer close(scooterCh)
+		var wg sync.WaitGroup
 
-		botResultCh := make([]<-chan simulatortype.SimulatorResponse, s.numberOfBots)
-		for i := 0; i < s.numberOfBots; i++ {
-			botResultCh[i] = s.ClientBot(scooterCh)
+		for _, sc := range scooters {
+			wg.Add(1)
+			s.RunClientBot(&wg, sc)
 		}
 
-		for _, scooter := range scooters {
-			select {
-			case <-s.ctx.Done():
-			case scooterCh <- scooter:
-			}
-		}
-
-		for r := range s.Fanin(botResultCh...) {
-			select {
-			case <-s.ctx.Done():
-			case resultChan <- r:
-			}
-		}
-
-	}()
-
-	return resultChan
-}
-
-func (s *Simulator) ClientBot(scooterCh <-chan clienttype.Scooter) <-chan simulatortype.SimulatorResponse {
-
-	resultCh := make(chan simulatortype.SimulatorResponse)
-
-	go func() {
-		defer close(resultCh)
-
-		for {
-
-			select {
-			case <-s.ctx.Done():
-				return
-			case sc, ok := <-scooterCh:
-				if !ok {
-					return
-				}
-
-				reserved, err := s.apiClient.ReserverScooter(clienttype.ReserveScooterRequestBody{
-					ScooterID: sc.ScooterID,
-				})
-				if err != nil {
-					s.SendResult(resultCh, simulatortype.SimulatorResponse{
-						Err: fmt.Errorf("StartClientBot: failed to reserve the scooter err= %w", err),
-					})
-					return
-				}
-
-				if !reserved {
-					s.SendResult(resultCh, simulatortype.SimulatorResponse{
-						Timestamp: time.Now(),
-						Message:   fmt.Sprintf("%v scooter is already inuse\n", sc.ScooterID),
-					})
-					return
-				}
-
-				nextLocation := sc.Location
-
-				// Send trip start event
-				if err := s.apiClient.PublishScooterStatus(clienttype.UpdateScooterRequestBody{
-					ScooterID: sc.ScooterID,
-					Timestamp: time.Now().Format(time.RFC3339),
-					Latitude:  fmt.Sprintf("%f", sc.Location[0]),
-					Longitude: fmt.Sprintf("%f", sc.Location[1]),
-					EventType: scooter.EVENT_TRIP_STARTED,
-				}); err != nil {
-					s.SendResult(resultCh, simulatortype.SimulatorResponse{
-						Err: err,
-					})
-					return
-				}
-				time.Sleep(3 * time.Second)
-
-				for i := 0; i < 3; i++ {
-					nextLocation = ride(nextLocation, 1, float64((60)*i))
-					if err := s.apiClient.PublishScooterStatus(clienttype.UpdateScooterRequestBody{
-						ScooterID: sc.ScooterID,
-						Timestamp: time.Now().Format(time.RFC3339),
-						Latitude:  fmt.Sprintf("%f", nextLocation[0]),
-						Longitude: fmt.Sprintf("%f", nextLocation[1]),
-						EventType: scooter.EVENT_TRIP_UPDATED,
-					}); err != nil {
-						s.SendResult(resultCh, simulatortype.SimulatorResponse{
-							Err: err,
-						})
-						return
-					}
-
-					time.Sleep(3 * time.Second)
-				}
-
-				nextLocation = ride(nextLocation, 1, 60)
-				// Send trip end event
-				if err := s.apiClient.PublishScooterStatus(clienttype.UpdateScooterRequestBody{
-					ScooterID: sc.ScooterID,
-					Timestamp: time.Now().Format(time.RFC3339),
-					Latitude:  fmt.Sprintf("%f", nextLocation[0]),
-					Longitude: fmt.Sprintf("%f", nextLocation[1]),
-					EventType: scooter.EVENT_TRIP_ENDED,
-				}); err != nil {
-					s.SendResult(resultCh, simulatortype.SimulatorResponse{
-						Err: err,
-					})
-					return
-				}
-				time.Sleep(3 * time.Second)
-
-				// Send scooter periodic update
-				if err := s.apiClient.PublishScooterStatus(clienttype.UpdateScooterRequestBody{
-					ScooterID: sc.ScooterID,
-					Timestamp: time.Now().Format(time.RFC3339),
-					Latitude:  fmt.Sprintf("%f", nextLocation[0]),
-					Longitude: fmt.Sprintf("%f", nextLocation[1]),
-					EventType: scooter.EVENT_PERIODIC_UPDATE,
-				}); err != nil {
-					s.SendResult(resultCh, simulatortype.SimulatorResponse{
-						Err: err,
-					})
-					return
-				}
-
-				s.SendResult(resultCh, simulatortype.SimulatorResponse{
-					Timestamp: time.Now(),
-					Message:   fmt.Sprintf("%v scooter trip ended\n", sc.ScooterID),
-				})
-
-			}
-		}
-
-	}()
-
-	return resultCh
-}
-
-func (s *Simulator) Fanin(chans ...<-chan simulatortype.SimulatorResponse) <-chan simulatortype.SimulatorResponse {
-
-	var wg sync.WaitGroup
-	multiplexedCh := make(chan simulatortype.SimulatorResponse)
-
-	multiplexer := func(ch <-chan simulatortype.SimulatorResponse) {
-		defer wg.Done()
-		for d := range ch {
-			select {
-			case <-s.ctx.Done():
-				return
-			case multiplexedCh <- d:
-			}
-		}
-	}
-
-	wg.Add(len(chans))
-
-	for _, ch := range chans {
-		go multiplexer(ch)
-	}
-
-	go func() {
-		defer close(multiplexedCh)
 		wg.Wait()
+
 	}()
 
-	return multiplexedCh
 }
 
-func (s *Simulator) SendResult(ch chan<- simulatortype.SimulatorResponse, r simulatortype.SimulatorResponse) {
-	select {
-	case <-s.ctx.Done():
-	case ch <- r:
+func (s *Simulator) RunClientBot(wg *sync.WaitGroup, sc clienttype.Scooter) error {
+
+	defer wg.Done()
+
+	reserved, err := s.apiClient.ReserverScooter(clienttype.ReserveScooterRequestBody{
+		ScooterID: sc.ScooterID,
+	})
+
+	if err != nil {
+		return fmt.Errorf("StartClientBot: failed to reserve the scooter err= %w", err)
 	}
+
+	if !reserved {
+		return fmt.Errorf(fmt.Sprintf("StartClientBot: %v scooter is already inuse\n", sc.ScooterID))
+	}
+
+	nextLocation := sc.Location
+
+	// Send trip start event
+	if err := s.apiClient.PublishScooterStatus(clienttype.UpdateScooterRequestBody{
+		ScooterID: sc.ScooterID,
+		Timestamp: time.Now().Format(time.RFC3339),
+		Latitude:  fmt.Sprintf("%f", sc.Location[0]),
+		Longitude: fmt.Sprintf("%f", sc.Location[1]),
+		EventType: scooter.EVENT_TRIP_STARTED,
+	}); err != nil {
+		return err
+	}
+	time.Sleep(3 * time.Second)
+
+	for i := 0; i < 3; i++ {
+		nextLocation = ride(nextLocation, 1, float64((60)*i))
+		if err := s.apiClient.PublishScooterStatus(clienttype.UpdateScooterRequestBody{
+			ScooterID: sc.ScooterID,
+			Timestamp: time.Now().Format(time.RFC3339),
+			Latitude:  fmt.Sprintf("%f", nextLocation[0]),
+			Longitude: fmt.Sprintf("%f", nextLocation[1]),
+			EventType: scooter.EVENT_TRIP_UPDATED,
+		}); err != nil {
+			return err
+		}
+
+		time.Sleep(3 * time.Second)
+	}
+
+	nextLocation = ride(nextLocation, 1, 60)
+	// Send trip end event
+	if err := s.apiClient.PublishScooterStatus(clienttype.UpdateScooterRequestBody{
+		ScooterID: sc.ScooterID,
+		Timestamp: time.Now().Format(time.RFC3339),
+		Latitude:  fmt.Sprintf("%f", nextLocation[0]),
+		Longitude: fmt.Sprintf("%f", nextLocation[1]),
+		EventType: scooter.EVENT_TRIP_ENDED,
+	}); err != nil {
+		return err
+	}
+	time.Sleep(3 * time.Second)
+
+	// Send scooter periodic update
+	if err := s.apiClient.PublishScooterStatus(clienttype.UpdateScooterRequestBody{
+		ScooterID: sc.ScooterID,
+		Timestamp: time.Now().Format(time.RFC3339),
+		Latitude:  fmt.Sprintf("%f", nextLocation[0]),
+		Longitude: fmt.Sprintf("%f", nextLocation[1]),
+		EventType: scooter.EVENT_PERIODIC_UPDATE,
+	}); err != nil {
+		return err
+	}
+
+	return nil
+
 }
 
 func ride(location clienttype.Location, distance, bearing float64) clienttype.Location {
